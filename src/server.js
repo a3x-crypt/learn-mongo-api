a6fc11ee76d7799093b3e816124c407c3cc6f0e6
atriple <andiaqil.dpe@gmail.com>
2020-02-09 09:02:32 +0700

    Trying some express req method and middlewares

--- src/server.js
+++ src/server.js
@@ -1,3 +1,30 @@
 import express from "express";
 
-const app = express();
+import { json } from "body-parser";
+
+export const app = express();
+
+app.use(json());
+
+const log = (req, res, next) => {
+  console.log("Logging");
+  next();
+};
+
+app.get("/", log, (req, res) => {
+  res.send({ message: "Hello!" });
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
+
+app.get("/user/help", (req, res) => {
+  res.send({ message: "Help" });
+});
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+};
